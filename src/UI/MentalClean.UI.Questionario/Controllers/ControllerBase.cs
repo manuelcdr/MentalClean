3e6421392f9379ad5d109499bfb2a1adbcc562c7
Manuel Cordeiro Neto <manuel.neto@ltm.digital>
2018-08-14 16:29:22 -0300

    add projeto ui

---
+++ src/UI/MentalClean.UI.Questionario/Controllers/ControllerBase.cs
@@ -0,0 +1,42 @@
+﻿using DDD.Infra.Cross.AspNetMvc.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalClean.UI.Questionario.Controllers
+{
+    public abstract class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+        protected IActionResult ResponseApi(object obj = null)
+        {
+            if (!ModelState.IsValid)
+                return Fail(obj);
+
+            return Success(obj);
+        }
+
+        protected IActionResult Success(object obj = null)
+        {
+            var objectReturn = new
+            {
+                success = true,
+                data = obj
+            };
+
+            return Ok(objectReturn);
+        }
+
+        protected IActionResult Fail(object obj = null)
+        {
+
+            var objectReturn = new
+            {
+                success = false,
+                fail = true,
+                errors = ModelState.GetErrors(),
+                data = obj
+            };
+
+            return Ok(objectReturn);
+        }
+
+    }
+}
---
+++ src/UI/MentalClean.UI.Questionario/Controllers/GenericController.cs
@@ -0,0 +1,186 @@
+﻿using AutoMapper;
+using DDD.Domain.Core.Interfaces.Entities;
+using DDD.Domain.Core.Interfaces.Repositories;
+using DDD.Domain.Core.Interfaces.Services;
+using DDD.Infra.Cross.DomainDriver;
+using DDD.Infra.Cross.DomainDriver.Attributes;
+using DDD.Infra.Cross.DomainDriver.Extensions;
+using DDD.Infra.Cross.DomainDriver.Mvc.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace MentalClean.UI.Questionario.Controllers
+{
+    public class GenericController : ControllerBase
+    {
+        private readonly IGenericRepositoryReadByName repositoryRead;
+        private readonly IGenericDomainService service;
+        private readonly IUnitOfWork uoW;
+
+        public GenericController(
+            IGenericRepositoryRead repositoryRead,
+            IGenericDomainService service,
+            IUnitOfWork uoW)
+            : base()
+        {
+            this.repositoryRead = repositoryRead;
+            this.service = service;
+            this.uoW = uoW;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return Ok("api inicializada");
+        }
+
+        [ObjectNameFilter("pessoa")]
+        [HttpGet("{objectName}")]
+        public IActionResult Get(string objectName)
+        {
+            ModelState.Clear();
+
+            var entidades = repositoryRead.GetAll(objectName);
+
+            var tipoModel = Driver.GetRefType("ViewModel", objectName, DriverAction.GetAll);
+
+            if (tipoModel == null)
+                return ResponseApi(entidades);
+
+            var models = Mapper.Map(entidades, entidades.GetType(), typeof(IEnumerable<>).MakeGenericType(tipoModel));
+            return ResponseApi(models);
+        }
+
+        [HttpGet("{objectName}/{id}")]
+        public IActionResult Get(string objectName, Guid id)
+        {
+            ModelState.Clear();
+
+            var entidade = repositoryRead.GetSingle(objectName, id);
+
+            var tipoModel = Driver.GetRefType("ViewModel", objectName, DriverAction.GetSingle);
+
+            if (tipoModel == null)
+                return ResponseApi(entidade);
+
+            var model = Mapper.Map(entidade, entidade.GetType(), tipoModel);
+            return ResponseApi(model);
+        }
+
+        [HttpPost("{objectName}")]
+        public IActionResult Post(string objectName, [FromBody] object value)
+        {
+            ModelState.Clear();
+
+            var vmType = Driver.GetRefType("ViewModel", objectName, DriverAction.Insert);
+            if (vmType == null)
+                return NotFound();
+
+            var domainType = Driver.GetDomainType(objectName);
+            var stringJsonObject = JsonConvert.SerializeObject(value);
+            var model = JsonConvert.DeserializeObject(stringJsonObject, vmType);
+
+            if (TryValidateModel(model))
+            {
+                var entity = JsonConvert.DeserializeObject(stringJsonObject, domainType) as IDefaultEntity;
+                service.Add(entity);
+                uoW.SaveChanges();
+            }
+
+            return ResponseApi(model);
+        }
+
+        [HttpPost("{objectName}/lot")]
+        public IActionResult PostLot(string objectName, [FromBody] object value)
+        {
+            ModelState.Clear();
+
+            var vmType = Driver.GetRefType("ViewModel", objectName, DriverAction.Insert);
+            if (vmType == null)
+                return NotFound();
+
+            var domainType = Driver.GetDomainType(objectName);
+            var stringJsonObject = JsonConvert.SerializeObject(value);
+            var models = JsonConvert.DeserializeObject(stringJsonObject, typeof(IEnumerable<>).MakeGenericType(vmType)) as IEnumerable<object>;
+
+            foreach (var model in models)
+            {
+                if (!TryValidateModel(model))
+                    return ResponseApi(models);
+
+                var entity = domainType.CreateInstance();
+                Mapper.Map(model, entity, vmType, domainType);
+                service.Add(entity as IDefaultEntity);
+            }
+
+            uoW.SaveChanges();
+            return Ok(models);
+        }
+
+        [HttpPut("{objectName}/{id}")]
+        public IActionResult Put(string objectName, Guid id, [FromBody] object value)
+        {
+            ModelState.Clear();
+
+            var vmType = Driver.GetRefType("ViewModel", objectName, DriverAction.Update);
+            if (vmType == null)
+                return NotFound();
+
+            var domainType = Driver.GetDomainType(objectName);
+
+            var stringJsonObject = JsonConvert.SerializeObject(value);
+            var model = JsonConvert.DeserializeObject(stringJsonObject, vmType);
+
+            if (TryValidateModel(model))
+            {
+                var entity = repositoryRead.GetSingle(objectName, id) as IDefaultEntity;
+                Mapper.Map(model, entity, vmType, domainType);
+                service.Update(entity);
+                uoW.SaveChanges();
+            }
+
+            return ResponseApi(model);
+        }
+
+        [HttpPut("{objectName}/lot")]
+        public IActionResult Put(string objectName, [FromBody] object value)
+        {
+            ModelState.Clear();
+
+            var vmType = Driver.GetRefType("ViewModel", objectName, DriverAction.Update);
+            if (vmType == null)
+                return NotFound();
+
+            var domainType = Driver.GetDomainType(objectName);
+
+            var stringJsonObject = JsonConvert.SerializeObject(value);
+            var models = JsonConvert.DeserializeObject(stringJsonObject, typeof(IEnumerable<>).MakeGenericType(vmType));
+
+            foreach (var model in models as IEnumerable<object>)
+            {
+                if (!TryValidateModel(model))
+                    return ResponseApi(models);
+
+                var entity = repositoryRead.GetSingle(objectName, (model as IId).GetId()) as IDefaultEntity;
+                Mapper.Map(model, entity, vmType, domainType);
+                service.Update(entity);
+                uoW.SaveChanges();
+            }
+
+            return ResponseApi(models);
+        }
+
+        [HttpDelete("{objectName}/{id}")]
+        public IActionResult Delete(string objectName, Guid id)
+        {
+            ModelState.Clear();
+
+            var type = Driver.GetDomainType(objectName);
+            service.Delete(type, id);
+            uoW.SaveChanges();
+            return ResponseApi(id);
+        }
+    }
+}
